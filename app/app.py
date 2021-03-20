17bd09f23eacf85dcf9021d5a8a38e10f3857457
marcioc0sta <marciocst2@gmail.com>
2021-03-20 15:36:49 -0300

    chore: use single quotes

--- app/app.py
+++ app/app.py
@@ -5,9 +5,9 @@ from pymongo import MongoClient
 app = Flask(__name__)
 api = Api(app)
 
-client = MongoClient("mongodb://db:27017")
+client = MongoClient('mongodb://db:27017')
 db = client.aNewDB
-UserNum = db["UserNum"]
+UserNum = db['UserNum']
 
 UserNum.insert_one({
     'num_of_users': 0
@@ -18,13 +18,13 @@ class Visit(Resource):
     def get(self):
         prev_num = UserNum.find({})[0]['num_of_users']
         new_num = prev_num + 1
-        UserNum.update({}, {"$set": {"num_of_users": new_num}})
-        return str(f"Hello user {new_num}")
+        UserNum.update({}, {'$set': {'num_of_users': new_num}})
+        return str(f'Hello user {new_num}')
 
 
 api.add_resource(Visit, '/hello')
 
 app.route('/')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0')

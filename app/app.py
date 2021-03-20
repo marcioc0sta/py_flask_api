0944e9a6585b188d5a55add439bdafb8b0bed092
marcioc0sta <marciocst2@gmail.com>
2021-03-20 16:29:58 -0300

    feat: add register user class

--- app/app.py
+++ app/app.py
@@ -1,28 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
+import bcrypt
 
 app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient('mongodb://db:27017')
-db = client.aNewDB
-UserNum = db['UserNum']
-
-UserNum.insert_one({
-    'num_of_users': 0
-})
+db = client.SentencesDB
+users = db['Users']
 
 
-class Visit(Resource):
-    def get(self):
-        prev_num = UserNum.find({})[0]['num_of_users']
-        new_num = prev_num + 1
-        UserNum.update({}, {'$set': {'num_of_users': new_num}})
-        return str(f'Hello user {new_num}')
+class Register(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        userName = postedData['username']
+        password = postedData['password']
+
+        hashedPw = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        users.insert_one({
+            "Username": userName,
+            "Password": hashedPw,
+            "Sentence": "",
+            "Tokens": 6
+        })
+
+        return jsonify({
+            "status": 200,
+            "msg": "You successfully signed up for the API"
+        })
 
 
-api.add_resource(Visit, '/hello')
+api.add_resource(Register, '/register')
 
 app.route('/')
 

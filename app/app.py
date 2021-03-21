c7710e4a99f4b2e9ea15acb2256f78237c0d21e8
marcioc0sta <marciocst2@gmail.com>
2021-03-21 16:20:12 -0300

    feat: add the basic features

--- app/app.py
+++ app/app.py
@@ -18,9 +18,9 @@ class Register(Resource):
         userName = postedData['username']
         password = postedData['password']
 
-        hashedPw = bcrypt.hashpw(password, bcrypt.gensalt())
+        hashedPw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
 
-        users.insert_one({
+        users.insert({
             "Username": userName,
             "Password": hashedPw,
             "Sentence": "",
@@ -33,7 +33,87 @@ class Register(Resource):
         })
 
 
+def verifyUser(username, password):
+    hashedPw = users.find({'Username': username})[0]["Password"]
+
+    if bcrypt.hashpw(password.encode('utf8'), hashedPw) == hashedPw:
+        return True
+    else:
+        return False
+
+
+def countUserTokens(username):
+    tokens = users.find({'Username': username})[0]["Tokens"]
+    return tokens
+
+
+class Store(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData['username']
+        password = postedData['password']
+        sentence = postedData['sentence']
+
+        correctPw = verifyUser(username, password)
+
+        if not correctPw:
+            return jsonify({
+                "status": 302,
+            })
+
+        num_tokens = countUserTokens(username)
+
+        if num_tokens <= 0:
+            return jsonify({
+                "status": 301,
+            })
+
+        users.update(
+            {'Username': username},
+            {"$set":  {
+                "Sentence": sentence,
+                "Tokens": num_tokens - 1
+            }})
+
+        return jsonify({
+            "status": 201,
+            "msg": "Sentence saved successfuly"
+        })
+
+
+class Get(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData['username']
+        password = postedData['password']
+
+        correctPw = verifyUser(username, password)
+
+        if not correctPw:
+            return jsonify({
+                "status": 302,
+            })
+
+        num_tokens = countUserTokens(username)
+
+        if num_tokens <= 0:
+            return jsonify({
+                "status": 301,
+            })
+
+        sentence = users.find({'Username': username})[0]['Sentence']
+
+        return jsonify({
+            "status": 200,
+            "sentence": sentence
+        })
+
+
 api.add_resource(Register, '/register')
+api.add_resource(Store, '/store')
+api.add_resource(Get, '/get')
 
 app.route('/')
 
